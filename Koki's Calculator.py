449d41b13c753db4a2ed68df2641f148429a0763
kokitajiri <kokitajiri.0124@gmail.com>
2025-09-03 19:51:55 +0900

    Added error checking to calculator.

--- Koki's Calculator.py
+++ Koki's Calculator.py
@@ -1,12 +1,35 @@
-num1 = float(input("Please enter the first number:"))
-cal = input("Please enter one of '+', '-', '*', or '/':")
-num2 = float(input("Please enter the second number:"))
-if cal == '+':
-    num3 = num1 + num2
-elif cal == '-': 
-        num3 = num1 - num2
-elif cal == '*': 
-        num3 = num1 * num2
-elif cal == '/': 
-        num3 = num1 / num2
-print("The answer is",num3)
+while True:
+    try:
+        number1 = float(input("Please enter the first number:"))
+        break
+    except ValueError:
+        print("Please enter the correct number.")
+
+while True:
+    calculator = input("Please enter one of '+', '-', '*', or '/':")
+    if calculator in ['+', '-', '*', '/']:
+        break
+
+    else:
+        print("Please enter the correct operator.")
+
+while True:
+    try:
+        number2 = float(input("Please enter the second number:"))
+        if calculator == '/' and number2 == 0:
+            print("You can't divide by 0")
+            continue
+        break
+    except ValueError:
+        print("Please enter the correct number.")
+
+if calculator == '+':
+    number3 = number1 + number2
+elif calculator == '-': 
+    number3 = number1 - number2
+elif calculator == '*': 
+    number3 = number1 * number2
+elif calculator == '/': 
+    number3 = number1 / number2
+print("The answer is",number3)
+
