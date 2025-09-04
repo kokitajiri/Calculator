9be9ee231347946313420871495e86b143d0397d
kokitajiri <kokitajiri.0124@gmail.com>
2025-09-04 19:46:17 +0900

    Added other calculators to koki's calculator file.

--- Kokis_calculator.py
+++ Kokis_calculator.py
@@ -1,3 +1,5 @@
+import math
+
 class Calc():
         #set up the class
     def __init__(self):
@@ -52,14 +54,116 @@ class Calc():
         elif self.operator == '/':
             return self.number1 / self.number2
 
-#Main function to run the calculator.
-def main():
-    calc = Calc()                   #Create a Calc object
-    calc.enter_number1()            #Get the first number
-    calc.enter_operator()           #Get the operator
-    calc.enter_number2()            #Get the second number
-    result = calc.calculate()       #Calculate result
-    print("The answer is",result)   #Show the result
 
+
+
+
+class FactorialCalc():
+    def __init__(self, number=None):
+        self.number = number
+        self.answer = None
+        
+    def enter_number(self):
+        while True:
+            try:
+                num = int(input("Please enter a positive integer:"))
+                if num < 0:
+                    print("The factorial of a negative number is undefined.")
+                    continue
+                self.number = num
+                return num
+            except ValueError:
+                print('Please enter the correct number.')
+                
+    def factorial_calculate(self):
+        if self.number == 0 or self.number == 1:
+            self.answer = 1
+            return 1
+            
+        else:
+            result = 1
+            for fac in range(2, self.number + 1):
+                result *= fac
+            self.answer = result
+            return result
+
+
+
+
+
+class PythagorasCalc():
+    def __init__(self, side1_length=None, side2_length=None):
+        self.side1_length = side1_length
+        self.side2_length = side2_length
+        self.answer = None
+        
+    def enter_side1(self):
+        while True:
+            try:
+                side1 = float(input("Please enter the first length of side:"))
+                if side1 <= 0:
+                    print("Please enter the correct length of side.")
+                    continue
+                self.side1_length = side1
+                return side1
+            except ValueError:
+                print("Please enter the correct length of side.")
+            
+    def enter_side2(self):
+        while True:
+            try:
+                side2 = float(input("Please enter the second length of side:"))
+                if side2 <= 0:
+                    print("Please enter the correct length of side.")
+                    continue
+                self.side2_length = side2
+                return side2
+            except ValueError:
+                print("Please enter the correct length of side.")
+            
+    def pythagoras_calculate(self):
+        number1 = self.side1_length ** 2
+        number2 = self.side2_length ** 2
+        number3 = number1 + number2
+        self.answer = math.sqrt(number3)
+        return self.answer
+        
+        
+        
+        
+        
+def main():
+    while True:
+        print("1. Calculator with four functions.(+, -, *, /)")
+        print("2.Factorial Calculator.")
+        print("3.Pythagoras Calculator.")
+        
+        select = input("select an option (1-3):")
+    
+        if select == "1":
+            calc = Calc()                   
+            calc.enter_number1()            
+            calc.enter_operator()           
+            calc.enter_number2()            
+            result = calc.calculate()      
+            print("The answer is",result) 
+        
+        elif select == "2":
+            calc = FactorialCalc()
+            calc.enter_number()
+            result = calc.factorial_calculate()
+            print("The answer is", result)
+        
+        elif select == "3":
+            calc = PythagorasCalc()
+            calc.enter_side1()
+            calc.enter_side2()
+            result = calc.pythagoras_calculate()
+            print("The length of the hypotenuse is", result)
+            break
+        
+        else:
+            print("Please enter the correct number.")
+        
 if __name__ == "__main__":
     main()
